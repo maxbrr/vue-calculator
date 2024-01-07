1015870247aac6fdedb772020df904b1f02e1f18
Max Brückner <maxbrckner@gmail.com>
2024-01-07 22:53:45 +0100

    Initial commit | created component files | imported fonts  | basic CSS-adjustments

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/CalculatorButtons.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div id="calculator-buttons">
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
